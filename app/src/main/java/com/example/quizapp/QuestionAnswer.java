f40e276ae25d42788cbf5e35a26256a9ac1ed660
MohammadRehanPatel <en20ca301008@medicaps.ac.in>
2022-05-30 12:18:39 +0530

    Quiz

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+
+    public static String question[]={
+      "Which one is not a Programming language",
+      "Php is an _______ Language",
+      "Which are the Java Tools",
+      "How Many  primitive Data types are in Java"
+
+    };
+    public static String choices[][]={
+            {"Oracle","Java","Python","C++"},
+            {"Complied","both a & c","Interpreted","None"},
+            {"JVM","JRE","JDK","All of the Above"},
+            {"5","7","8","2"}
+
+    };
+    public static  String correctAnswers[]={
+            "Oracle",
+            "Interpreted",
+            "All of the Above",
+            "8"
+    };
+}
